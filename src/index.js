31ce2094785bcb67587d97588036aa4023587e58
BendelVolodymyr <bendel.vova@gmail.com>
2023-10-27 18:11:02 +0200

    push

--- src/index.js
+++ src/index.js
@@ -4,10 +4,66 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { getSearch } from './js/search-api';
 
+
+
 const formEl = document.querySelector('#search-form');
 const buttonLoadEl = document.querySelector('.load-more');
-console.log(buttonLoadEl)
+const galleryEl = document.querySelector('.gallery');
+
+let resultPage = 0;
+
+function onSearc(event) {
+    event.preventDefault();
+    
+    console.log(event)
+    const formValue = formEl.elements.searchQuery.value;
+    if (formValue.length == 0) return alert('Форма не може бути пуста');
+    resultPage += 1;
+    getSearch(formValue, resultPage).then(data => {
+    const arrayData = data.data.hits;
+    if (arrayData.length == 0) return Notiflix.Notify.info("Sorry, there are no images matching your search query. Please try again.");
+    Notiflix.Notify.success('true');
+    return createDomEl(arrayData);
+}
+)
+    .catch(err => Notiflix.Notify.failure(err.message))
+    .finally();
+   
+}
+formEl.addEventListener('submit', onSearc);
 
 
-getSearch();
+function createDomEl(arrayData) { 
+    
+    return galleryEl.innerHTML = arrayData.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
+    `<div class="photo-card">
+ <a href="${largeImageURL}" class="gallery__link"  ><img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+  <div class="info">
+    <p class="info-item">
+      <b>likes:${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views:${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments:${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads:${downloads}</b>
+    </p>
+  </div>
+</div>`).join('');
+    
+    
+}
+
+var lightbox = new SimpleLightbox('.gallery.photo-card a', {
+    sourceAttr: 'href', // завантаження файла з ...
+    overlayOpacity: 0.4, // прозорість фону
+    animationSpeed: 500, // Анімація перемикання слайдів (швидкість)
+    captionsData: 'alt', // додаємо з опису
+    captionPosition: 'bottom', // позиція
+    captionDelay: 250, // затримка опису '250 ms'
+});
+
 
--- src/js/search-api.js
+++ src/js/search-api.js
@@ -3,10 +3,10 @@ import axios, { Axios } from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '40252530-8571e952a05bfbe082a85c49d';
 
-export async function getSearch(searchValue, pageNew,) {
+export async function getSearch(searchValue, pageNew) {
 const params = new URLSearchParams({
     key: API_KEY,
-    q: 'cat',
+    q: searchValue,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
@@ -14,13 +14,6 @@ const params = new URLSearchParams({
     per_page: 40,
 })
     
-    try {
-        await axios.get(`${BASE_URL}?${params}`)
-        .then(response => {
-            console.log(response);
-        })
-    } catch (err) {
-        console.log(err);
-    }
+   return await axios.get(`${BASE_URL}?${params}`);
      
 }
