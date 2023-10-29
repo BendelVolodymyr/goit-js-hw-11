47eb9afd910a581939585a07ab8e3ac64fdf8998
BendelVolodymyr <bendel.vova@gmail.com>
2023-10-29 07:44:13 +0100

    test push

--- src/index.js
+++ src/index.js
@@ -12,12 +12,13 @@ const galleryEl = document.querySelector('.gallery');
 
 let resultPage = 1;
 let formValue = '';
+let createNewDomEl = '';
 
 async function onSearc(event) {
     event.preventDefault();
      formValue = formEl.elements.searchQuery.value;
     if (formValue.length == 0) return alert('Форма не може бути пуста');
-    
+    resultPage = 1;
     await getSearch(formValue, resultPage).then(data => {
         const arrayData = data.data.hits;
     if (arrayData.length == 0) return Notiflix.Notify.info("Sorry, there are no images matching your search query. Please try again.");
@@ -33,8 +34,8 @@ async function onSearc(event) {
 formEl.addEventListener('submit', onSearc);
 buttonLoadEl.addEventListener('click', onLoader);
 
-function createDomEl(arrayData) { 
-  galleryEl.innerHTML = arrayData.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
+function test(arrayData) {
+ return arrayData.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
     `<div class="photo-card">
         <a class="gallery__link" href="${largeImageURL}"  ><img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
   <div class="info">
@@ -52,6 +53,12 @@ function createDomEl(arrayData) {
     </p>
   </div>
 </div>`).join('');
+}
+
+function createDomEl(arrayData) { 
+  createNewDomEl = test(arrayData);
+  galleryEl.innerHTML = test(arrayData);
+  
   var lightbox = new SimpleLightbox('.gallery a', {
     sourceAttr: 'href',
     overlayOpacity: 0.4, 
@@ -66,7 +73,8 @@ async function onLoader() {
     resultPage += 1;
     await getSearch(formValue, resultPage).then(data => {
         const arrayData = data.data.hits;
-        createDomEl(arrayData);
+      createNewDomEl += test(arrayData);
+      galleryEl.innerHTML = createNewDomEl;
     })
 }
 
